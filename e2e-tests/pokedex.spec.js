3fdb36815dd854df409c8db0b34938ad7480ab2b
KingEliTheBoss <eliazar.j@hotmail.com>
2025-04-03 16:48:29 -0600

    Ex. 9 E2E tests part 2

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -6,4 +6,11 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+
+  test('can navigate from main page to specific pokémon page', async ({ page }) => {
+    await page.goto('/')
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
 })
